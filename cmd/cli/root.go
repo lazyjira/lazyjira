03c1bc3391b0e88ce758425e14ce6481bed8f2c2
Vaugen Wakeling <55480610+vaugenwake@users.noreply.github.com>
2024-06-25 19:19:48 +0100

    Refactor to handlers pattern

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -1,12 +1,8 @@
 package cli
 
 import (
-	"errors"
 	"fmt"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/matthewrobinsondev/lazyjira/pkg/config"
-	"github.com/matthewrobinsondev/lazyjira/pkg/jira"
-	"github.com/matthewrobinsondev/lazyjira/pkg/tui"
+	"github.com/matthewrobinsondev/lazyjira/pkg/handlers"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -14,21 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "lazyjira",
 	Short: "For devs who put code before POs",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cfg, err := config.Load()
-		if err != nil {
-			return errors.New(fmt.Sprintf("Error loading configuration: %v", err))
-		}
-
-		jiraClient := jira.NewClient(cfg)
-		p := tea.NewProgram(tui.NewTuiModel(jiraClient), tea.WithAltScreen())
-
-		if _, err := p.Run(); err != nil {
-			return errors.New(fmt.Sprintf("Could not start the program: %v\n", err))
-		}
-
-		return nil
-	},
+	RunE:  handlers.NewLazyJiraHandler,
 }
 
 func Execute() {
