88534e9ab7262623ce35f2b773a1b6ae9cfde166
Vaugen Wakeling <55480610+vaugenwake@users.noreply.github.com>
2024-06-25 19:15:58 +0100

    Refactor to using cobra and a command interface

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"os"
-
-	"github.com/matthewrobinsondev/lazyjira/pkg/config"
-	"github.com/matthewrobinsondev/lazyjira/pkg/jira"
-	"github.com/matthewrobinsondev/lazyjira/pkg/tui"
-
-	tea "github.com/charmbracelet/bubbletea"
-)
+import "github.com/matthewrobinsondev/lazyjira/cmd/cli"
 
 func main() {
-	cfg, err := config.Load()
-	if err != nil {
-		log.Fatalf("Error loading configuration: %v", err)
-	}
-
-	jiraClient := jira.NewClient(cfg)
-	p := tea.NewProgram(tui.NewTuiModel(jiraClient), tea.WithAltScreen())
-
-	if _, err := p.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Could not start the program: %v\n", err)
-		os.Exit(1)
-	}
+	cli.Execute()
 }
